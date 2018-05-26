c389d251947e9402580909c92f76d7a2a505bd4f
Yael Brodsky <yael@codaxis.com>
2018-05-26 17:33:20 -0300

    smour de mierda

---
+++ bot-messages.js
@@ -0,0 +1,291 @@
+module.exports = {
+  cron: {
+    '9': [
+      'Garrlnnggg, otra vez empezaste a llegar tarde?',
+      'Gerling estas cagando?',
+      'Solo aca eh....',
+      'un tema tranquilo para empezar el dia https://www.youtube.com/watch?v=5abamRO41fE ',
+      [{
+        msg: 'Augusto',
+        delay: 0,
+      }, {
+        msg: 'tenes para prestarme el flotador?',
+        delay: 2
+      }, {
+        msg: 'rico de mierda',
+        delay: 5,
+      }],
+      [{
+        msg: '8:45 am ya me tocaron los huevos con un mail al mejor estilo siderca',
+        delay: 0,
+      }, {
+        msg: 'tantas ganas tienen de que sea un lunes pija',
+        delay: 10
+      }],
+    ],
+    '16': [
+      'que suerte algunos que ya se van',
+      'me acuerdo cuando era pobre, a esta hora todavia me quedaba una horita de burreo',
+      [{
+        msg: 'bueno, me voy a echar un cago y tomar un cafe.',
+        delay: 2,
+      }, {
+        msg: 'Mi meta es media hora',
+        delay: 4,
+      }]
+    ],
+    '15': [
+        'me voy a dormir una siesta, esclavos',
+        'bueno, se termina el dia para mi que soy rico',
+    ],
+  },
+  mentions: {
+    // el marica
+    U8QPXKU76: [
+      'cerra el ojete Augusto, haceme el favor',
+      'rico inmundo',
+      'que cojida de hermana que te hace falta a vos',
+      'segui golpeando perros vos',
+      ['estoy viendo de comprar unas remeras', 'si te interesa'],
+    ],
+    // el gdo
+    U8Q8BSMQ9: [
+      'Cerra el ogt Gerling',
+      'estas cagando Gerling?',
+      'se ve que la vieja no esta hoy',
+      'no entendi',
+      'Gaaaaaaaaaaaaaaarleeeen',
+      'fijate, mas o menos',
+      'desconozco',
+      'depende para quien',
+      'no se mucho del tema',
+      'cansado',
+      'renuncia marica',
+      'Que ojete que tiene esa Milena',
+      'andas con ganas de matarlo se ve',
+      'sabes lo que te falta a vos para ser paria',
+      'porteño',
+      'el cbu?',
+      'Alejandro "el cuatro botellas" Gerling',
+      'que argentino que resultaste',
+      [{
+        msg: 'eraa tu rol en el viaje',
+        delay: 0
+      }, {
+        msg: 'atar las cosas, mad dj skills',
+        delay: 20,
+      }, {
+        msg: 'y guiarme',
+        delay: 22,
+      }, {
+        msg: 'https://imgur.com/gallery/ISh6m67',
+        delay: 25,
+      }],
+      'te vino Gerling?',
+      'andas en minita',
+    ]
+  },
+  channels: {
+    // random
+    C8QPXKZPW: [
+      'me compre un jugo marca dia por $10. Que veneno que es, pero que barato',
+      'anoche hicimos 50 vueltas, 600 dominadas, 1235 abdominales y 47 sentadillas. estoy muerto',
+      'me voy a echar un cago de media hora',
+      'esa Ceci, como gasta guita',
+      [{
+        msg: 'Este fin de semana capaz voy para alla.',
+        delay: 0,
+      }, {
+        msg: 'Gerling, como esta tu agenda?',
+        delay: 10,
+      }, {
+        msg: 'Deja, me voy con Mica mejor',
+        delay: 15,
+      }],
+      ['recien me desperte', 'me dormi una siesta'],
+      'fijate, hasta que esten echas',
+      'ese gol pija. 7000 me costo arreglarle la caja',
+      'hay que cojerle a la hermana',
+      [{
+        msg: 'estoy viendo de irme unos dias a la costa',
+        delay: 0,
+      }, {
+        msg: 'si Mica no quiere ir, a uds les interesa?',
+        delay: 3,
+      }],
+      [{
+        msg: 'ayer compre 2kg de cerdo, 1kg de vacio, 14kg de lomo, 2 gallinas y un pavo real por $100',
+        delay: 0,
+      }, {
+        msg: 'voy a empezar con lo de la cetosis de nuevo',
+        delay: 10,
+      }, {
+        msg: 'espero esta vez no estar al borde de la muerte',
+        delay: 15,
+      }],
+      'yo soy un tipo muy querido',
+      [{
+        msg: 'yo nunca pedi delivery',
+        delay: 0,
+      }, {
+        msg: 'siempre fui a buscarlo',
+        delay: 3,
+      }, {
+        msg: 'si pido algo lo voy a buscar en bici',
+        delay: 5,
+      }],
+      'una playera tenes gerling, callate mejor',
+      'que hincha pelotas que esta mi vieja',
+      'pueden creer que se me quedo el auto pija otra vez?',
+      'este Pedro, que tipo infeliz',
+      'hoy me pongo a revelar las fotos',
+      [{
+        msg: 'deja de decir huevadas',
+        delay: 0,
+      },{
+        msg: 'que decaido que esta esto',
+        delay: 10,
+      }, {
+        msg: 'como elastico de calzon de Gerling',
+        delay: 20,
+      }],
+      [{
+        msg: 'esos postrecitos dia',
+        delay: 0,
+      }, {
+        msg: 'son los danettes de los paria',
+        delay: 5,
+      }],
+      [{
+        msg: 'que calor la cajeta del sol',
+        delay: 0,
+      }, {
+        msg: 'que lindo seria estar abajo de una parra',
+        delay: 10,
+      }, {
+        msg: 'tomando un exprimido de naranja',
+        delay: 15,
+      }],
+      'el dia que no sea pobre el dinero no va a afectar mis decisiones',
+      [{
+        msg: 'Gerling, si la vieja te deja...',
+        delay: 0,
+      }, {
+        msg: 'contanos como quedo la cosa',
+        delay: 10,
+      }],
+      [{
+        msg: 'queres algo bueno... tenes que nacer hijo de rico',
+        delay: 0,
+      }, {
+        msg: 'tipo los hijos de tinelli',
+        delay: 5,
+      }, {
+        msg: 'y dps te la tiras de rebelde',
+        delay: 10,
+      }],
+      [{
+        msg: 'no es facil cambiar',
+        delay: 0,
+      }, {
+        msg: 'no es facil reconocer el error propio y mejorar',
+        delay: 5,
+      }, {
+        msg: 'paciencia',
+        delay: 10,
+      }],
+      [
+          'que asco una porno en 4k',
+          'tantos detalles innecesarios',
+          { msg: 'fluidos, poros', delay: 10 },
+      ],
+      'llego a casa y me hago un batido de proteina',
+      [{
+        msg: 'que lleno de manjares que esta gesell',
+        delay: 0,
+      }, {
+        msg: 'de manjares y de cautriciclos',
+        delay: 5,
+      }, {
+        msg: 'y de manjares en cuatriciclo',
+        delay: 7,
+      }],
+      [{
+        msg: 'Pinche el colchon.... la primer noche, colchon pija',
+        delay: 0,
+      }, {
+        msg: 'mi puteada tipo 2 am fue: colchon pija, te aguantas a gerling y te venis a pinchara ahora',
+        delay: 5
+      }],
+      [{
+        msg: 'me paso por el saco de los huevos al resto del mundo jajaja',
+        delay: 0,
+      }, {
+        msg: 'me he cansado de putear el sagrado imen de la virgen maria frente a todos',
+        delay: 5
+      }],
+      [{
+        msg: 'yo tengo ganas de estar pescando o tirado abajo de un arbol, afilando un cuchillo',
+        delay: 0,
+      }, {
+        msg: 'al pedo, pero afilandolo',
+        delay: 5
+      }],
+      [{
+        msg: 'esta vida',
+        delay: 0,
+      }, {
+        msg: 'que quita y que da',
+        delay: 5
+      }, {
+        msg: 'easy comes, easy goes',
+        delay: 10,
+      }],
+      [{
+        msg: 'el otro dia con mica compramos un pan lactal don yeyo para hacer unos sanguchitos en la playa',
+        delay: 0,
+      }, {
+        msg: 'es aserrin apelmasado',
+        delay: 5
+      }],
+      [{
+        msg: 'no se porque te ofendes cuando digo que los de contabilidad de siderca son imbeciles',
+        delay: 0,
+      }, {
+        msg: 'uno de ellos es la gorda lorena, la que viajaba con nosotros y la rajaron de todos los grupos de viaje por idiota ',
+        delay: 10
+      }],
+      [{
+        msg: 'podes cree que me llamaron cuando estaba comiendo',
+        delay: 0,
+      }, {
+        msg: 'me re cago en los de contabilidad',
+        delay: 10
+      }],
+      'por que mejor no me palmeas las bolas hasta que suenen como el feliz cumpleaños?',
+    ],
+    // compañeros de trabajo
+    C8U60NRC7: [
+      'Gerling, como anda Seba?',
+      'tranquilo gerlingceo',
+      'Como anda la vieja, volvio a llevar torta?',
+      [{
+        msg: 'que judio que es mi viejo',
+        delay: 0,
+      }, {
+        msg: 'todo el dia sin parar',
+        delay: 2,
+      }, {
+        msg: 'le gusta mandarse la parte',
+        delay: 5,
+      }, {
+        msg: 'no almuerza y despues anda haciendose la victima. Comiendo un sanguche a las apuradas',
+        delay: 10,
+      }, {
+        msg: 'al pedo',
+        delay: 20,
+      }],
+      'que lindo que se siente ser judio rico',
+    ]
+  }
+};
